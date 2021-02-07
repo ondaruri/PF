75e35c11d444362dd2b42cd6b5f1a69fc907bcbf
ondaruri <f3raser8cg@yahoo.co.jp>
2021-02-07 05:07:35 +0000

    いいね機能（非同期ではない）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,16 @@
 class LikesController < ApplicationController
 
   def create
+    post_ramen= PostRamen.find(params[:post_ramen_id])
+    like = current_user.likes.new(post_ramen_id: post_ramen.id)
+    like.save
+    redirect_to post_ramen_path(post_ramen)
   end
 
   def destroy
+    post_ramen = PostRamen.find(params[:post_ramen_id])
+    like = current_user.likes.find_by(post_ramen_id: post_ramen.id)
+    like.destroy
+    redirect_to post_ramen_path(post_ramen)
   end
 end
