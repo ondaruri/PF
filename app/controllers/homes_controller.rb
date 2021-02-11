ef198268ac5cd4fdf350ea657420082b459446dc
ondaruri <f3raser8cg@yahoo.co.jp>
2021-02-11 16:07:13 +0000

    検索機能修正（AND、OR検索）

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
     @q = PostRamen.ransack(params[:q])
     @tags = ActsAsTaggableOn::Tag.all
     @post_ramens = PostRamen.all.order("created_at DESC")
+    @post_ramen = PostRamen.page(params[:page]).all.per(3)
     # @post_ramens = @q.result
     # @post_ramens = @search_value.result(distinct: true)
   end
