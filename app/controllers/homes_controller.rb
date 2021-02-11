eec3e20621513cd418c886faf2f8694ff297de97
ondaruri <f3raser8cg@yahoo.co.jp>
2021-02-11 08:46:58 +0000

    トップページカード実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,7 @@ class HomesController < ApplicationController
   def top
     @q = PostRamen.ransack(params[:q])
     @tags = ActsAsTaggableOn::Tag.all
+    @post_ramens = PostRamen.all.order("created_at DESC")
     # @post_ramens = @q.result
     # @post_ramens = @search_value.result(distinct: true)
   end
