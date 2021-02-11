194a384dbdba1b3ba38f184f3ace6b0fc88229d3
ondaruri <f3raser8cg@yahoo.co.jp>
2021-02-11 04:01:38 +0000

    検索機能

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @q = PostRamen.ransack(params[:q])
+    @tags = ActsAsTaggableOn::Tag.all
     # @post_ramens = @q.result
     # @post_ramens = @search_value.result(distinct: true)
   end
