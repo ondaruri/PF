e328a0e2a4a7c9d67a1d9740642b1619bf325b5d
ondaruri <f3raser8cg@yahoo.co.jp>
2021-02-17 13:41:22 +0000

    コメント非同期

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,9 +4,9 @@ class PostCommentsController < ApplicationController
   end
 
   def create
-    post_ramen = PostRamen.find(params[:post_ramen_id])
+    @post_ramen = PostRamen.find(params[:post_ramen_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.post_ramen_id = post_ramen.id
+    comment.post_ramen_id = @post_ramen.id
     comment.save
     # redirect_to post_ramen_path(post_ramen)
     # redirect_to root_path
@@ -23,8 +23,8 @@ class PostCommentsController < ApplicationController
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], post_ramen_id: params[:post_ramen_id]).destroy
-    redirect_to post_ramen_path(params[:post_ramen_id])
+    @post_ramen = PostRamen.find(params[:post_ramen_id])
+    @post_comment = PostComment.find_by(id: params[:id], post_ramen_id: params[:post_ramen_id]).destroy
   end
 
   private
