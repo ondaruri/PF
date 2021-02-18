c0bc343fcebcfec70d8d71267b4b00290e9b4e6a
ondaruri <f3raser8cg@yahoo.co.jp>
2021-02-18 13:23:03 +0000

    途中保存

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,13 @@ class HomesController < ApplicationController
     @tags = ActsAsTaggableOn::Tag.all
     @post_ramens = PostRamen.all.order("created_at DESC")
     @post_ramen = PostRamen.page(params[:page]).all.per(3)
-    # @post_ramens = @q.result
-    # @post_ramens = @search_value.result(distinct: true)
+    # -----------------right--------------------------------
+    @post_ramen_all_ranks = PostRamen.create_all_ranks
+    @most_used_tags = ActsAsTaggableOn::Tag.most_used(5)
+    
   end
 end
+
+
+
+
