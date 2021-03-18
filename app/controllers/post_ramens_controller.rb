b4b1f8dab7b7ef2b8a90fb3ebfcc5dc24ed30f29
ondaruri <f3raser8cg@yahoo.co.jp>
2021-03-18 23:15:12 +0000

    README

--- app/controllers/post_ramens_controller.rb
+++ app/controllers/post_ramens_controller.rb
@@ -21,7 +21,7 @@ class PostRamensController < ApplicationController
     @q = PostRamen.ransack(params[:q])  #rightサイドの変数↓↓
     @tags = ActsAsTaggableOn::Tag.all
     @post_ramen_all_ranks = PostRamen.create_all_ranks #いいね全期間ランキング
-    @post_ramen_month_ranks = PostRamen.create_month_ranks #いいね週間ランキング
+    @post_ramen_month_ranks = PostRamen.create_month_ranks #いいね月間間ランキング
     @post_ramen_year_ranks = PostRamen.create_year_ranks
     @most_used_tags = ActsAsTaggableOn::Tag.most_used(10)
     now = Time.current
